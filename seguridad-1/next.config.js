e2df4d35e1f890d8189a97daa71aa685eab10b8a
Mauricio-Bernuy <54604331+Mauricio-Bernuy@users.noreply.github.com>
2023-07-18 01:25:37 -0500

    final push

--- seguridad-1/next.config.js
+++ seguridad-1/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self' https://www.investors.com/ 'unsafe-eval' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  child-src 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' https://fonts.googleapis.com/ 'unsafe-eval' 'unsafe-inline';
+  font-src 'self' https://fonts.googleapis.com/ https://fonts.gstatic.com/;
+`
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -8,7 +17,33 @@ const nextConfig = {
       aggregateTimeout: 300
     }
     return config
-  }
+  },
+  // Adding policies:
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()  
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
